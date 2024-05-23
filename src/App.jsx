0a3eb62a29fd56719b4633c4c8333bdafd5dcce5
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-05-23 17:24:16 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='main-app'>
+      <header className="app-header">
+        <section className="container">
+          <h1>Mister Email</h1>
+        </section>
+      </header>
+
+      <main className='container'>
+        {/* <Home /> */}
+      </main>
+
+      <footer>
+        <section className="container">
+          Iris Oron &copy;
+        </section>
+      </footer>
+    </section>
+  )
+}
+
+export default App
