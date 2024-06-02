07ded3e1f1ee0b36cdd2ae0547b7e6bc59161a1e
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-06-02 20:46:49 +0200

    Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,24 @@
 import { NavLink } from 'react-router-dom'
+import logoUrl from '../assets/images/io-mail1.svg'
 
 export function HomePage() {
 	return (
 		<section className='home-page'>
-			<div className="home__logo"></div>
-			<div className="home__text">
+			<div className="home__logo">
+				<img src={logoUrl} alt="logo" />
+			</div>
+			<div className="home__text-header">
 				<h1>I am writing a letter</h1>
 				<h2>I’ve come to the end of the page</h2>
-				<h3>To keep you from waiting forever,</h3>
+			</div>
+			<div className="home__text-content">
+				<h3>To avoid having you wait forever,</h3>
 				<h3>you’re welcome to engage!</h3>
 			</div>
-			<div className="home__enter">
+			<div className="home__enter button">
 				<NavLink to='/mail/inbox'>Enter</NavLink>
 			</div>
-			<div className='home__about'>
+			<div className='home__about button'>
 				<NavLink to='/about'>About</NavLink>
 			</div>
 		</section>
